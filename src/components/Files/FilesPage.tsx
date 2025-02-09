c08f2fe901b4024409d3fffc500549ff69ed18cd
Neovasky <neovasky@gmail.com>
2025-02-09 00:13:17 -0500

    idk. Code Works

---
+++ src/components/Files/FilesPage.tsx
@@ -0,0 +1,10 @@
+/**
+ * File: src/pages/Files/FilesPage.tsx
+ * Description: Main files page container
+ */
+
+import { FilesLayout } from "../../components/Files";
+
+export const FilesPage = () => {
+  return <FilesLayout />;
+};
